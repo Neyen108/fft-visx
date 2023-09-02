211b1e9d5ad21314e09c6b2b0b85596895c3d0fe
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2023-09-03 01:33:35 +0530

    feat: accidentally created a music player.

--- main.c
+++ main.c
@@ -2,9 +2,24 @@
 #include "include/raylib.h"
 
 int main(void){
+	int width = 800;
+	int height = 600;
+	InitWindow(width, height, "fft-visx");
+	SetTargetFPS(60);
+	
 	InitAudioDevice();
+	Music music = LoadMusicStream("Ollie_Seasons.mp3");
+	//Sound sound = LoadSound("Ollie_Seasons.mp3");
+	PlayMusicStream(music);
+	
+	while (!WindowShouldClose()) {
+		UpdateMusicStream(music);
+		BeginDrawing();
+		ClearBackground(RED);
+		EndDrawing();
+	}
+	
 
-	Sound sound = LoadSound("Ollie_Seasons.mp3");
 	return 0;
 }
 
